3ab33b58c03709cb5a23112768c3ebd58f56d7eb
Peter Avgerinos <108369084+PeterAvgerinos@users.noreply.github.com>
2022-10-22 18:30:58 +0300

    l

--- gameboard.py
+++ gameboard.py
@@ -1,5 +1,17 @@
 import pygame
-from .pygameconstants import BLACK, ROWS, SQUARE_SIZE, FPS, WIN, RED
+
+WIDTH, HEIGHT = 600, 600
+ROWS, COLS = 8, 8
+SQUARE_SIZE = WIDTH//COLS
+FPS = 60
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+
+#RGB Colors
+RED = (255, 0, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
+GREY = (128,128,128)
 
 class Board:
     def __init__(self):
