7650c4988c29cfd899e7d052ebfd57d9bddc269e
Peter Avgerinos <108369084+PeterAvgerinos@users.noreply.github.com>
2022-10-25 12:46:47 +0300

    l

--- vis/mecanix/board.py
+++ vis/mecanix/board.py
@@ -13,26 +13,26 @@ class Board:
     #         for col in range(row % 2, ROWS, 2):
     #             pygame.draw.rect(win, RED, (row*SQUARE_SIZE, col*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
 
-    # def draw_squares(self, win):
-    #     win.fill(BLACK)
-    #     k = ROWS - 1
-    #     for row in range(ROWS):
-    #         for col in range(k):
-    #             pygame.draw.rect(win, BLACK, (row*SQUARE_SIZE, col*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
-    #
-    #         k = k - 1
-    #
-    #         for col in range(0, row + 1):
-    #             pygame.draw.rect(win, RED, (row*SQUARE_SIZE, col*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
-
     def draw_squares(self, win):
         win.fill(BLACK)
-        k = COLS - 1
-        n = 1
+        k = ROWS - 1
         for row in range(ROWS):
-            for col in range(k//2):
+            for col in range(k):
                 pygame.draw.rect(win, BLACK, (row*SQUARE_SIZE, col*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
-            for col in range(n):
+
+            k = k - 1
+
+            for col in range(0, row + 1):
                 pygame.draw.rect(win, RED, (row*SQUARE_SIZE, col*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
-            for col in range(k//2):
-                pygame.draw.rect(win, BLACK, (row*SQUARE_SIZE, col*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+
+    # def draw_squares(self, win):
+    #     win.fill(BLACK)
+    #     k = COLS - 1
+    #     n = 1
+    #     for row in range(ROWS):
+    #         for col in range(k//2):
+    #             pygame.draw.rect(win, BLACK, (row*SQUARE_SIZE, col*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+    #         for col in range(n):
+    #             pygame.draw.rect(win, RED, (row*SQUARE_SIZE, col*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+    #         for col in range(k//2):
+    #             pygame.draw.rect(win, BLACK, (row*SQUARE_SIZE, col*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
