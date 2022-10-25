444e67658ebe4df07a354443beeaee42e6ad3208
Peter Avgerinos <108369084+PeterAvgerinos@users.noreply.github.com>
2022-10-25 17:10:40 +0300

    l

--- vis/mecanix/board.py
+++ vis/mecanix/board.py
@@ -1,5 +1,5 @@
 import pygame
-from .constants import BLACK, ROWS, COLS, RED, SQUARE_SIZE
+from .constants import BLACK, ROWS, COLS, RED, SQUARE_SIZE, WHITE, BLUE
 
 class Board:
     def __init__(self):
@@ -13,26 +13,27 @@ class Board:
     #         for col in range(row % 2, ROWS, 2):
     #             pygame.draw.rect(win, RED, (row*SQUARE_SIZE, col*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
 
+    # def draw_squares(self, win):
+    #     win.fill(BLACK)
+    #     k = ROWS - 1
+    #     for row in range(ROWS):
+    #         for col in range(k):
+    #             pygame.draw.rect(win, RED, (row*SQUARE_SIZE, col*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+    #
+    #         k = k - 1
+    #
+    #         for col in range(k):
+    #             pygame.draw.rect(win, RED, (row*SQUARE_SIZE, col*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+
     def draw_squares(self, win):
         win.fill(BLACK)
         k = ROWS - 1
         for row in range(ROWS):
-            for col in range(k):
-                pygame.draw.rect(win, BLACK, (row*SQUARE_SIZE, col*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+            for col in range(COLS):
+                if (k//2) < COLS//2 - 1:
+                    pygame.draw.rect(win, RED, (row*SQUARE_SIZE, col*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+                elif k//2 == COLS//2 - 1:
+                    pygame.draw.rect(win, WHITE, (row*SQUARE_SIZE, col*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+                else:
+                    pygame.draw.rect(win, RED, (row*SQUARE_SIZE, col*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
 
-            k = k - 1
-
-            for col in range(k + 1):
-                pygame.draw.rect(win, RED, (row*SQUARE_SIZE, col*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
-
-    # def draw_squares(self, win):
-    #     win.fill(BLACK)
-    #     k = COLS - 1
-    #     n = 1
-    #     for row in range(ROWS):
-    #         for col in range(k//2):
-    #             pygame.draw.rect(win, BLACK, (row*SQUARE_SIZE, col*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
-    #         for col in range(n):
-    #             pygame.draw.rect(win, RED, (row*SQUARE_SIZE, col*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
-    #         for col in range(k//2):
-    #             pygame.draw.rect(win, BLACK, (row*SQUARE_SIZE, col*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
