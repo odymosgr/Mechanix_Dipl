3fef355af4bb0d2710134fe5452c8eddc77009d0
Peter Avgerinos <108369084+PeterAvgerinos@users.noreply.github.com>
2022-11-07 17:50:14 +0200

    l

--- vis/main.py
+++ vis/main.py
@@ -2,6 +2,8 @@ import pygame
 from mecanix.constants import WIDTH, HEIGHT
 from mecanix.board import Board
 
+pygame.init()
+
 FPS = 60
 
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -11,6 +13,9 @@ def main():
     run = True
     clock = pygame.time.Clock()
     board = Board()
+    board.draw_squares(WIN)
+    board.draw_gear_mounts(WIN)
+    board.create_board(WIN)
 
     while run:
         clock.tick(FPS)
@@ -22,8 +27,7 @@ def main():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 pass
 
-        board.draw_squares(WIN)
-        board.draw_gear_mounts(WIN)
+        board.update()
         pygame.display.update()
 
 
