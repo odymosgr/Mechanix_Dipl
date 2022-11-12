83f500f0dcafd315c2c63b43937c275aa60c4583
Peter Avgerinos <108369084+PeterAvgerinos@users.noreply.github.com>
2022-11-12 23:32:50 +0200

    l

--- vis/mecanix/board.py
+++ vis/mecanix/board.py
@@ -1,13 +1,19 @@
 import pygame
-from .constants import BLACK, BLUEGEAR, RED, TARGETGEAR, WIDTH, SQUARE_SIZE, MOUNT_SIZE, ROWS, BLUEGEAR, TARGETGEAR
+from .constants import BLACK, RED, TARGETGEAR, TRANSPARENT, WIDTH, SQUARE_SIZE, MOUNT_SIZE, ROWS, BLUEGEAR, TARGETGEAR, GREENGEAR
 from .gear import Gear
 
 class Board(pygame.sprite.Sprite):
     def __init__(self):
         self.board = []
         self.gear_group = pygame.sprite.Group()
-        self.selected_piece = None
+        self.selected_gear = None
         self.green_left = self.blue_left = 12
+
+    def __repr__(self):
+        for row in range(ROWS):
+            print('row = ',row)
+            for col in range(row + 1):
+                print(self.board[row][col])
 
     def draw_squares(self, win):
         for row in range(ROWS):
@@ -27,17 +33,36 @@ class Board(pygame.sprite.Sprite):
                     pygame.draw.circle(win, BLACK, (WIDTH//2 - SQUARE_SIZE//2 - (row - col)*SQUARE_SIZE + 2*MOUNT_SIZE, SQUARE_SIZE*row + 2*MOUNT_SIZE + 50), MOUNT_SIZE)
 
     def create_board(self, win):
+        self.draw_squares(win)
+        self.draw_gear_mounts(win)
         for row in range(ROWS):
             self.board.append([])
             for col in range(row + 1):
                 if row != 0:
-                    new_gear = Gear(row, col, 'transparent', WIDTH//2 - SQUARE_SIZE//2 - (row - col)*SQUARE_SIZE + 2*MOUNT_SIZE + (SQUARE_SIZE//2)*row,  SQUARE_SIZE*row + 2*MOUNT_SIZE + 50, BLUEGEAR)
+                    new_gear = Gear(row, col, 'transparent', WIDTH//2 - SQUARE_SIZE//2 - (row - col)*SQUARE_SIZE + 2*MOUNT_SIZE + (SQUARE_SIZE//2)*row,  SQUARE_SIZE*row + 2*MOUNT_SIZE + 50, TRANSPARENT)
+                    if row == ROWS - 1:
+                        if col == 0:
+                            new_gear = Gear(row, col, 'blue', WIDTH//2 - SQUARE_SIZE//2 - (row - col)*SQUARE_SIZE + 2*MOUNT_SIZE + (SQUARE_SIZE//2)*row,  SQUARE_SIZE*row + 2*MOUNT_SIZE + 50, BLUEGEAR)
+                            new_gear.make_fixed()
+                        elif col == ROWS-1:
+                            new_gear = Gear(row, col, 'green', WIDTH//2 - SQUARE_SIZE//2 - (row - col)*SQUARE_SIZE + 2*MOUNT_SIZE + (SQUARE_SIZE//2)*row,  SQUARE_SIZE*row + 2*MOUNT_SIZE + 50, GREENGEAR)
+                            new_gear.make_fixed()
                 else:
-                    new_gear = Gear(row, col, 'transparent', WIDTH//2 - SQUARE_SIZE//2 - (row - col)*SQUARE_SIZE + 2*MOUNT_SIZE,  SQUARE_SIZE*row + 2*MOUNT_SIZE + 50, TARGETGEAR)
+                    new_gear = Gear(row, col, 'target', WIDTH//2 - SQUARE_SIZE//2 - (row - col)*SQUARE_SIZE + 2*MOUNT_SIZE,  SQUARE_SIZE*row + 2*MOUNT_SIZE + 50, TARGETGEAR)
+                    new_gear.make_fixed()
                 self.board[row].append(new_gear)
                 self.gear_group.add(new_gear)
-                # new_gear.draw(win)
         self.gear_group.draw(win)
 
-    def update_board(self):
-        self.gear_group.update()
+    def update_board(self, win):
+        self.gear_group.draw(win)
+
+    def move(self, gear, color, win):
+        selected_gear = self.board[gear.row][gear.col]
+        if not selected_gear.fixed:
+            selected_gear.set_color(color)
+        self.update_board(win)
+
+    def get_gear(self, row, col):
+        return self.board[row][col]
+
